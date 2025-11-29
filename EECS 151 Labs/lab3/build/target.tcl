11b21e670b7450546b8ba1e1d3cbf735b98a1bcd
shridhay <47260704+shridhay@users.noreply.github.com>
2025-11-28 23:21:04 -0500

    Add files via upload

---
+++ EECS 151 Labs/lab3/build/target.tcl
@@ -0,0 +1,7 @@
+set ABS_TOP                         /home/cc/eecs151/fa23/class/eecs151-aer/fpga-labs-fa23-shridhay/lab3
+set TOP                            z1top
+set FPGA_PART                      xc7z020clg400-1
+set_param general.maxThreads       4
+set_param general.maxBackupLogs    0
+set RTL { /home/cc/eecs151/fa23/class/eecs151-aer/fpga-labs-fa23-shridhay/lab3/src/button_parser.v /home/cc/eecs151/fa23/class/eecs151-aer/fpga-labs-fa23-shridhay/lab3/src/counter.v /home/cc/eecs151/fa23/class/eecs151-aer/fpga-labs-fa23-shridhay/lab3/src/dac.v /home/cc/eecs151/fa23/class/eecs151-aer/fpga-labs-fa23-shridhay/lab3/src/debouncer.v /home/cc/eecs151/fa23/class/eecs151-aer/fpga-labs-fa23-shridhay/lab3/src/edge_detector.v /home/cc/eecs151/fa23/class/eecs151-aer/fpga-labs-fa23-shridhay/lab3/src/sq_wave_gen.v /home/cc/eecs151/fa23/class/eecs151-aer/fpga-labs-fa23-shridhay/lab3/src/synchronizer.v /home/cc/eecs151/fa23/class/eecs151-aer/fpga-labs-fa23-shridhay/lab3/src/z1top.v }
+set CONSTRAINTS { /home/cc/eecs151/fa23/class/eecs151-aer/fpga-labs-fa23-shridhay/lab3/src/z1top.xdc }
